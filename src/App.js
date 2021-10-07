93edd3decba8f45bb392d7582fbc06dbc7ebcdfd
bipinrai054 <bipinrai054@gmail.com>
2021-10-07 18:51:26 +0545

    homepage layout done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
+import HomePage from './HomePage-component';
 import './App.css';
 
 function App() {
-  return <div className='App'>Hello World!</div>;
+  return (
+    <div className='App'>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
