40689a99b52124d0df9f33b2ff658bb3f0582646
bipinrai054 <bipinrai054@gmail.com>
2021-10-14 00:17:43 +0545

    card-item created and connected with card dropdown with card item reducer

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from '../custom-button/CustomButton';
+import CartItem from '../cart-item/CartItem';
 import './CartDropdown.scss';
 
-function CartDropdown() {
+function CartDropdown({ cartItems }) {
   return (
     <div className='cart-dropdown'>
-      <div className='cart-items' />
+      <div className='cart-items'>
+        {cartItems.map((cartItem) => {
+          return <CartItem key={cartItem.id} item={cartItem} />;
+        })}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 }
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  return {
+    cartItems,
+  };
+};
+
+export default connect(mapStateToProps)(CartDropdown);
